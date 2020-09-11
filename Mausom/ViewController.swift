af2da4f04261c53bb877f032db3865ede7fe4579
Siddharth Pande <siddharth.v.pande@gmail.com>
2020-09-11 19:10:59 +0530

    Initial Commit

---
+++ Mausom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mausom
+//
+//  Created by Siddharth on 11/09/20.
+//  Copyright © 2020 SP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
